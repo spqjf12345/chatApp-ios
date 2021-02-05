2190f1e88f7cf0be200d108f418c631179ee0f22
URI <spqjf12345@gmail.com>
2021-02-05 20:40:30 +0900

    first commit

---
+++ chat_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat_App
+//
+//  Created by JoSoJeong on 2021/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
